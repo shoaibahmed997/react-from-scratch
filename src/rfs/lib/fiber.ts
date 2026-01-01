80b8efb5d5e52e663b68e7cc682876e30e35cc93
shoaibahmed997 <falakshabir123@gmail.com>
2026-01-02 01:16:10 +0530

    perform unit of work

--- src/rfs/lib/fiber.ts
+++ src/rfs/lib/fiber.ts
@@ -1,13 +1,19 @@
 import { createDom } from "./dom";
-import { Fiber, Element } from "./types";
+import { Fiber, Element, Props } from "./types";
 import { deletions } from "./render";
 
 // performs a single fiber. so far by my understanding it creates a dom for a fiber. and select the next fiber for work
 // the way it selects is  traverse to child -> when child finishes -> work upwards and create dom for siblings -> keep going up until the root node.
 export function performUnitOfWork({ fiber }: { fiber: Fiber }): Fiber | null {
-  if (!fiber.dom) {
-    fiber.dom = createDom({ fiber });
+  const isFunctionComponent = typeof fiber.type === "function";
+
+  if (isFunctionComponent) {
+    updateFunctionComponent({ fiber });
+  } else {
+    updateHostComponent({ fiber });
   }
+
+  if (fiber.child) return fiber.child;
 
   reconcileChildren({
     wipFiber: fiber,
@@ -24,6 +30,29 @@ export function performUnitOfWork({ fiber }: { fiber: Fiber }): Fiber | null {
   return null;
 }
 
+function updateFunctionComponent({ fiber }: { fiber: Fiber }): void {
+  // setwipfiber
+  // resetwipfiberhooks
+
+  const children = [(fiber.type as (props: Props) => Element)(fiber.props)];
+
+  reconcileChildren({
+    wipFiber: fiber,
+    elements: children,
+  });
+}
+
+function updateHostComponent({ fiber }: { fiber: Fiber }): void {
+  if (!fiber.dom) {
+    fiber.dom = createDom({ fiber });
+  }
+
+  reconcileChildren({
+    wipFiber: fiber,
+    elements: fiber.props.children || [],
+  });
+}
+
 export function reconcileChildren({
   wipFiber, // current fiber
   elements, // its children fiber.props.children
@@ -32,9 +61,11 @@ export function reconcileChildren({
   elements: Array<Element>;
 }): void {
   let index = 0;
-  let oldFiber = wipFiber.alternate?.child || null;
-  let prevSibling: Fiber | null = null;
+  let oldFiber = wipFiber.alternate?.child || null; // points to the first child of previous version of fiber
+  let prevSibling: Fiber | null = null; // used to link new siblings in fiber tree.
 
+  // this loop parallely checks the new fiber's children with old children and decides if we need to update, add or delete the fibers
+  // The goal of reconcileChildren is to compare the new set of children (elements) with the previous set (fibers) and build a new fiber tree that represents the next state of the UI. This process is called "reconciliation" or "diffing."
   while (index < elements.length || oldFiber !== null) {
     const element = elements[index];
 
@@ -42,7 +73,7 @@ export function reconcileChildren({
 
     const isSametype = oldFiber && element && element.type === oldFiber.type;
 
-    // what is the difference between the oldFiber and wipFiber? they are basically different versions of fiber. oldFiber is the fiber that was rendered on the last rendering cycle and wipFiber is the current fiber with maybe some new changes
+    // what is the difference between the oldFiber and wipFiber? oldFiber is previous set(version) child of wipFiber and element is the new child of wipFiber
 
     // if the oldFiber exists and oldfiber type is same as the element type
     // what is element? element here is the child of Root fiber for first iteration of performUnitOfWork
@@ -75,11 +106,11 @@ export function reconcileChildren({
       newFiber = {
         type: element.type,
         props: element.props,
-        dom: null,
+        dom: null, // new element can't reuse the dom
         parent: wipFiber,
         child: null,
         sibling: null,
-        alternate: null,
+        alternate: null, // no alternate since it's new
         effectTag: "PLACEMENT", // -> i don't know what this is but as per my understanding this will be used later when doms are appended to the html
       };
     }
@@ -103,6 +134,7 @@ export function reconcileChildren({
       prevSibling.sibling = newFiber;
     }
 
-    prevSibling = newFiber; // we are setting this for the new loop iteration.
+    prevSibling = newFiber; // we are setting this for the next loop iteration.
+    index++;
   }
 }
